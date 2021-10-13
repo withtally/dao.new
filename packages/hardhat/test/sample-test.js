e01e59a83fa6655986cb2c6225c64a9f6bf0f6b8
Elad Mallel <eladmallel@gmail.com>
2021-10-13 14:04:14 +0300

    fix project config and code to make sample test work

--- packages/hardhat/test/sample-test.js
+++ packages/hardhat/test/sample-test.js
@@ -2,13 +2,13 @@ const { expect } = require("chai");
 
 describe("Greeter", function() {
   it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
+    const Greeter = await ethers.getContractFactory("YourContract");
     const greeter = await Greeter.deploy("Hello, world!");
     
     await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    expect(await greeter.greeting()).to.equal("Hello, world!");
 
     await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await greeter.greeting()).to.equal("Hola, mundo!");
   });
 });
