30b62cdcac59689529f34eecba1ac9665b198e7f
Elad Mallel <eladmallel@gmail.com>
2021-11-21 17:39:24 +0200

    solve shared pkg build warnings

--- packages/shared-frontend/rollup.config.js
+++ packages/shared-frontend/rollup.config.js
@@ -1,18 +1,25 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2'
 
-import pkg from "./package.json";
+import pkg from './package.json'
 
 export default {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true,
       strict: false,
     },
   ],
   plugins: [typescript()],
-  external: ["react", "react-dom", "@chakra-ui/react", "@chakra-ui/icons"],
-};
+  external: [
+    'react',
+    'react-dom',
+    '@chakra-ui/react',
+    '@chakra-ui/icons',
+    '@chakra-ui/button',
+    'react/jsx-runtime',
+  ],
+}
