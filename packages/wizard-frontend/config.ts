a67c3d6a4d44d77d34b8449c45b8f9095caa0680
David Brailovsky <davidbrai@gmail.com>
2021-11-22 15:37:49 +0100

    tally api url in per chain config

--- packages/wizard-frontend/config.ts
+++ packages/wizard-frontend/config.ts
@@ -6,6 +6,10 @@ export const CHAIN_ID: SupportedChains = ChainId.Rinkeby
 
 export const secrets = { alchemyApiKey: process.env.ALCHEMY_API_KEY }
 
-export const tallyApiURI = 'http://localhost:5000/query'
-// export const tallyApiURI = 'https://api2.withtally.com/query'
+const tallyApiURIConfig: Record<SupportedChains, string> = {
+  [ChainId.Localhost]: 'http://localhost:5000/query',
+  [ChainId.Rinkeby]: 'https://api2.withtally.com/query',
+}
+
+export const tallyApiURI = tallyApiURIConfig[CHAIN_ID]
 export const tallyWebBaseURI = 'https://hackathon.withtally.com/governance/'
