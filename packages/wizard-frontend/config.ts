02e7d9b99fd0be92fb81017766dd85fa04c832ba
David Brailovsky <davidbrai@gmail.com>
2021-12-02 15:36:49 +0100

    change config back to rinkeby

--- packages/wizard-frontend/config.ts
+++ packages/wizard-frontend/config.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@usedapp/core'
 
 type SupportedChains = ChainId.Rinkeby | ChainId.Localhost
 
-export const CHAIN_ID: SupportedChains = ChainId.Localhost
+export const CHAIN_ID: SupportedChains = ChainId.Rinkeby
 
 export const secrets = {
   alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
