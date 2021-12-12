b29067705ee62d1302a45fb7e63c4fbe4c2c9533
David Brailovsky <davidbrai@gmail.com>
2021-12-12 17:21:33 +0100

    cleanup

--- packages/contracts-frontend/components/RaribleLink.tsx
+++ packages/contracts-frontend/components/RaribleLink.tsx
@@ -5,7 +5,6 @@ import { config } from '../config'
 export const RaribleLink = ({ linkText }) => {
   const { chainId } = useEthers()
 
-  // TODO: support mainnet
   let raribleLink = ''
   if (chainId === ChainId.Rinkeby) {
     raribleLink = `https://rinkeby.rarible.com/collection/${config.tokenAddress}`
