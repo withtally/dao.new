dd5d5d024d26ed2763a8144c07fe04db6cd0291f
David Brailovsky <davidbrai@gmail.com>
2021-12-13 10:18:49 +0100

    cleanup

--- packages/contracts-frontend/pages/_app.tsx
+++ packages/contracts-frontend/pages/_app.tsx
@@ -9,24 +9,16 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import { CHAIN_ID, multicallOnLocalhost } from '../config'
 
-// scaffold-eth's INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
-
 const config: Config = {
   readOnlyChainId: CHAIN_ID,
   readOnlyUrls: {
-    [ChainId.Ropsten]: `https://ropsten.infura.io/v3/${INFURA_ID}`,
     [ChainId.Rinkeby]: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
     [ChainId.Hardhat]: 'http://localhost:8545',
     [ChainId.Localhost]: 'http://localhost:8545',
   },
   supportedChains: [
     ChainId.Mainnet,
-    ChainId.Goerli,
-    ChainId.Kovan,
     ChainId.Rinkeby,
-    ChainId.Ropsten,
-    ChainId.xDai,
     ChainId.Localhost,
     ChainId.Hardhat,
   ],
