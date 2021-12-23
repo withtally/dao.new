f609df9961fb5ae54ec02b81bd490e5d9d67efc7
David Brailovsky <davidbrai@gmail.com>
2021-12-23 15:31:24 +0100

    update wallet connect rpc urls

--- packages/shared-frontend/src/lib/connectors.ts
+++ packages/shared-frontend/src/lib/connectors.ts
@@ -1,12 +1,14 @@
+import { ChainId } from '@usedapp/core'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS: { [chainId: number]: string } = {
-  1: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
-  4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
+  [ChainId.Rinkeby]: process.env.NEXT_PUBLIC_RINKEBY_JSON_RPC,
+  [ChainId.Mainnet]: process.env.NEXT_PUBLIC_MAINNET_JSON_RPC,
+  [ChainId.OptimismKovan]: 'https://kovan.optimism.io',
 }
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
+  rpc: RPC_URLS,
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
 })
