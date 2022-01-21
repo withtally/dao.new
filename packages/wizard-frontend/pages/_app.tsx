345ee378a921b3a8e529db35838012254066b074
David Brailovsky <davidbrai@gmail.com>
2022-01-21 10:42:04 +0100

    add google analytics tag

--- packages/wizard-frontend/pages/_app.tsx
+++ packages/wizard-frontend/pages/_app.tsx
@@ -14,6 +14,7 @@ import {
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { multicallOnLocalhost } from '../config'
+import Script from 'next/script'
 
 const config: Config = {
   readOnlyChainId: ChainId.Rinkeby,
@@ -42,6 +43,25 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <DAppProvider config={config}>
       <ChakraProvider>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </ChakraProvider>
     </DAppProvider>
