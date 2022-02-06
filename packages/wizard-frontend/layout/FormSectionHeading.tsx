0ddfbfd0a5b954c7425d2a4d81e7dd986a44f087
David Brailovsky <davidbrai@gmail.com>
2022-02-06 18:14:40 +0100

    wizard ui: softer headings

--- packages/wizard-frontend/layout/FormSectionHeading.tsx
+++ packages/wizard-frontend/layout/FormSectionHeading.tsx
@@ -2,7 +2,7 @@ import { Heading, Text } from '@chakra-ui/react'
 
 export const FormSectionHeader = ({ number, text }): JSX.Element => {
   return (
-    <Heading as="h2" display="flex" fontSize="36px" mb="26px">
+    <Heading as="h2" display="flex" fontSize="36px" mb="26px" fontWeight={600}>
       <Text color="brand.300">{number}.&nbsp;</Text>
       <Text color="darkText.100">{text}</Text>
     </Heading>
