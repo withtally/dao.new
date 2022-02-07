4a446618616963b448e9bd3589b759bb92ae7a61
David Brailovsky <davidbrai@gmail.com>
2022-02-07 15:48:53 +0100

    wizard ui: soften headings

--- packages/wizard-frontend/layout/FormSectionHeading.tsx
+++ packages/wizard-frontend/layout/FormSectionHeading.tsx
@@ -4,7 +4,7 @@ export const FormSectionHeader = ({ number, text }): JSX.Element => {
   return (
     <Heading as="h2" display="flex" fontSize="36px" mb="26px" fontWeight={600}>
       <Text color="brand.300">{number}.&nbsp;</Text>
-      <Text color="darkText.100">{text}</Text>
+      <Text color="brand.100">{text}</Text>
     </Heading>
   )
 }
